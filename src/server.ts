62bab7ae9cef5b21750451083b350b74f2d57178
Armand DEBESSE <73818074+armanddebesse@users.noreply.github.com>
2023-06-28 22:36:37 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,8 @@ dotenv.config();
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI as string, { useNewUrlParser: true, useUnifiedTopology: true } as any)
-  .then(() => console.log('Successfully connected to MongoDB.'));
+  .then(() => console.log('Successfully connected to MongoDB.'))
+  .catch((error) => console.log('Failed to connect to MongoDB.', error));
 
 initLapinou();
 
