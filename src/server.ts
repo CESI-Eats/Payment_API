d6297f5fd3289ddb5b6c1c861e24a5c167e0a209
Armand DEBESSE <73818074+armanddebesse@users.noreply.github.com>
2023-06-28 17:46:12 +0200

    Fix/db error (#4)
    * Add new pipelines
    
    * [FIX]db error
    
    ---------
    
    Co-authored-by: FW11-GMIOT/gmiot001 <guillaumemiot.gm@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,7 @@ dotenv.config();
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI as string, { useNewUrlParser: true, useUnifiedTopology: true } as any)
-  .then(() => console.log('Successfully connected to MongoDB.'))
-  .catch((error) => console.log('Failed to connect to MongoDB.', error));
+  .then(() => console.log('Successfully connected to MongoDB.'));
 
 initLapinou();
 
